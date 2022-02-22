b22e2e46ac5810d90acb3fe98f9bcdd19ec238ac
Lauren <sincerelyolivia@outlook.com>
2022-02-22 18:26:20 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,11 +1,1 @@
-terraform {
-  required_providers { 
-    r = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
-  }
-
-
-  required_version = ">= 0.14.0"
-}
+#emptying this file to test bug
